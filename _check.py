1a7b9bc29693a79b1bcc29fea9214eec2cd6fa02
YujiSX <80231823@qq.com>
2026-06-06 16:08:38 +0800

    chore: 清理临时文件，完善 .gitignore

--- _check.py
+++
@@ -1,1 +0,0 @@
-﻿import json; f=open(r"D:\slay the\CosmicPrincessKaguya\kaguya\Kaguya\localization\zhs\ancients.json","r",encoding="utf-8"); c=f.read(); f.close(); print(len(c)); json.loads(c); print("OK")
